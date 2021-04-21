f8a208cbb8241d89a21d376e3e85de33d8a995e3
Brian Whiteley <brianwhiteleybw@gmail.com>
2021-04-21 13:52:03 -0500

    add character types

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,6 +1,24 @@
-
+import string
 
 class PasswordGenerator:
+    LOWERCASE = string.ascii_lowercase
+    UPPERCASE = string.ascii_uppercase
+    NUMBERS = "".join([str(i) for i in range(0,10)])
 
-    def add_password(self, pw):
-        self.pw = pw
+
+    def __init__(self):
+        self.pw = "password"
+        self.length = 10
+        self.characters = []
+
+    def generate_password(self):
+        pass
+
+    def get_password(self):
+        return self.pw
+
+    def set_password_length(self, length):
+        self.length = length
+
+    def add_characters(self, character_type):
+        self.characters.append(character_type)
--- test_passwordGenerator.py
+++ test_passwordGenerator.py
@@ -1,14 +1,33 @@
 import unittest
 from passwordGenerator import PasswordGenerator
+import string
 
 class TestPasswordGenerator(unittest.TestCase):
     @classmethod
     def setUp(self):
         self.pg = PasswordGenerator()
     
-    def test_addPassword(self):
-        self.pg.add_password("newPassword")
+    def test_generate_password(self):
+        self.pg.generate_password()
 
+    def test_get_password(self):
+        pw = self.pg.get_password()
+        self.assertEqual("password", pw)
+
+    def test_set_password_length(self):
+        self.pg.set_password_length(12)
+        length = self.pg.length
+        self.assertEqual(12, length)
+
+    def test_default_password_length(self):
+        length = self.pg.length
+        self.assertEqual(10, length)
+
+    def test_add_characters(self):
+        self.pg.add_characters(self.pg.LOWERCASE)
+        characters = string.ascii_lowercase
+        self.assertIn(characters, self.pg.characters)
+        
 
 if __name__ == '__main__':
     unittest.main()
